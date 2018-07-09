35ff5a259d6091a56317125a1a6e9efbdab47e36
BukinPK <bukinpk@gmail.com>
2018-07-10 02:19:57 +0300

    start

---
+++ btbot/bounty.py
@@ -0,0 +1,16 @@
+
+class Socials:
+
+    def __init__(self):
+        twitter
+        facebook
+        telegram
+    
+class Bounty:
+
+    def __init__(self, id, social, week_start=0, start=None, end=None):
+        self.id = id
+        self.social = social
+        self.week_start = week_start
+        self.start = start
+        self.end = end
